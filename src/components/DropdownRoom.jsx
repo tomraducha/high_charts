20f914e553c1607aa01cc6ee37914ffbec0f3418
traducha <traducha@vayandata.com>
2023-04-17 12:26:06 +0200

    refactor(useState): delete selectedOption

--- src/components/DropdownRoom.jsx
+++ src/components/DropdownRoom.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function DropdownRoom({ options, onSelect }) {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
-
   function handleOptionSelect(option) {
-    setSelectedOption(option);
     onSelect(option);
   }
 
   return (
-    <select value={selectedOption} onChange={(e) => handleOptionSelect(e.target.value)}>
+    <select onChange={(e) => handleOptionSelect(e.target.value)}>
       {options.map((option) => (
         <option key={option} value={option}>
           Pollux
